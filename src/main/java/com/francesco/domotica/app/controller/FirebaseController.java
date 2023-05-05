4615ae021d90d1e35e82e497d424a0dbdbc306a2
francesco.fabbri <francesco.fabbri@sitrack.com>
2023-05-05 02:20:41 -0500

    add CORS

--- src/main/java/com/francesco/domotica/app/controller/FirebaseController.java
+++ src/main/java/com/francesco/domotica/app/controller/FirebaseController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://comforting-blini-f8927b.netlify.app/")
 public class FirebaseController {
 
     @Autowired
